9e589bb6cb02433bd147c20a52381a5892caa460
kejriwalrahul <kejriwalrahul@outlook.com>
2019-01-12 15:14:21 +0530

    added result caching

--- pyfunctor.py
+++ pyfunctor.py
@@ -2,13 +2,15 @@
 	To Do:
 		1. explore stream-in stream-out data
 		2. cacheability of results
-		3. type-checking
 
 	Requirements for parallel execution:
 		1. __name__ == "__main__" is reqd
 		2. Functors should be defined at module top level
 """
 
+import os, errno
+import pickle
+import ctypes
 from tqdm import tqdm
 from functools import partial, wraps
 from multiprocessing import Pool
@@ -19,8 +21,12 @@ from contextlib import contextmanager
 	Config
 """
 NUM_THREADS = 2
-DEBUG = False
+CACHEDIR = "data_cache/"
 
+
+"""
+	Helpers
+"""
 
 @contextmanager
 def PoolContextWrapper(thing):
@@ -29,39 +35,73 @@ def PoolContextWrapper(thing):
     finally:
         thing.terminate()
 
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+hash_file_path = lambda x: CACHEDIR + str(x) + ".pkl"
+
+def hex_hash(val):
+	return hex(ctypes.c_size_t(hash(val)).value)
+
+def next_hash(hashval, functor_name):
+	return hex_hash(str(hashval) + functor_name)
 
 """
 	Abstract Data Representation that can be manipulated using custom Functors
 """
 class AbstractSpace:
 
-	def __init__(self, data):
+	def __init__(self, data, hashval="", parallelize=True):
 		self.data = data
+		self.hash = hex_hash(hashval)
+		self.parallelize = parallelize
 
 	def values(self):
 		return self.data
 
 	def apply(self, func, *args, **kwargs):
-		if DEBUG:
-			self.serial_apply(func, *args, **kwargs)
+		if self.parallelize:
+			self.__serial_apply(func, *args, **kwargs)
 		else:
-			self.parallel_apply(func, *args, **kwargs)
+			self.__parallel_apply(func, *args, **kwargs)
+		self.hash = next_hash(self.hash, func.__name__)
 		return self
 
-	def serial_apply(self, func, *args, **kwargs):
+	def __serial_apply(self, func, *args, **kwargs):
 		partial_func = partial(func, *args, **kwargs)
 		self.data = map(partial_func, tqdm(self.data))
 
-	def parallel_apply(self, func, *args, **kwargs):
+	def __parallel_apply(self, func, *args, **kwargs):
 		partial_func = partial(func, *args, **kwargs)
 		with PoolContextWrapper(Pool(processes=NUM_THREADS)) as pool:
 			self.data = list(tqdm(pool.imap(partial_func, self.data), total=len(self.data)))
+
+	def cache_data(self, hashval, functor_name):
+		mkdir_p(CACHEDIR)
+		hpath = hash_file_path(next_hash(hashval, functor_name))
+		with open(hpath, "wb") as cache_file:
+			pickle.dump((self.data, self.hash), cache_file)
+
+	def read_cache(self, functor_name):
+		hpath = hash_file_path(next_hash(self.hash, functor_name))
+		if os.path.exists(hpath):
+			with open(hpath, "rb") as cache_file:
+				self.data, self.hash = pickle.load(cache_file)
+				return True
+		return False
 
 
 """
 	Builds Functors for manipulating AbstractSpace
 """
 def functorize(func):
+
+	func = sanitizor(func)
 	
 	@wraps(func)
 	def wrapper(self, *args, **kwargs):
@@ -75,9 +115,31 @@ def functorize(func):
 	Builds Functor Compositions
 """
 def pipeliner(func):
+
+	@wraps(func)
 	def wrapper(self, *args):
 		return func(*args, el=self)
+
 	setattr(AbstractSpace, func.__name__, wrapper)
+
+
+"""
+	Builds Functor Compositions which cache output results for reuse
+"""
+def cache_pipeliner(func):
+
+	@wraps(func)
+	def wrapper(self, *args):
+		if not self.read_cache(func.__name__):
+			orig_hash = self.hash
+			return_val = func(*args, el=self)
+			self.cache_data(orig_hash, func.__name__)
+			return return_val
+		else:
+			return self			
+
+	setattr(AbstractSpace, func.__name__, wrapper)	
+
 
 """
 	Try-Catch Wrapper to prevent errors from stopping entire processing
--- test.py
+++ test.py
@@ -1,25 +1,29 @@
-from pyfunctor import AbstractSpace, functorize, sanitizor, pipeliner
-
-# import pyfunctor
-# pyfunctor.DEBUG = True
+from pyfunctor import AbstractSpace, functorize, pipeliner, cache_pipeliner
+import math
 
 @functorize
-@sanitizor
 def adder(x, el):
 	if el == 5:
 		raise NotImplementedError
 	return el + x
 
 @functorize
-@sanitizor
 def multiplier(y, el):
 	return el * y
+
+@functorize
+def sigmoid(el):
+	return 1.0 / (1 + math.exp(el))
 
 @pipeliner
 def LinearTransformPipeline(x, y, el):
 	return el.multiplier(x).adder(y)
 
+@cache_pipeliner
+def SigmoidNeuronTransform(x, y, el):
+	return el.LinearTransformPipeline(x, y).sigmoid()
+
 if __name__ == "__main__":
  
-	space = AbstractSpace([1,2,3])
-	print space.LinearTransformPipeline(5,3).values()
+	space = AbstractSpace([1,2,3], parallelize=True, hashval="testvals")
+	print space.SigmoidNeuronTransform(5,3).values()
